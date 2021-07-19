3d09273d3809170bba2a74ede172ca01a99b05b2
Joel Lee <70297362+Dolebannas@users.noreply.github.com>
2021-07-19 17:20:07 -0400

    Add files via upload

---
+++ Vehicle/Raspi_detection.py
@@ -0,0 +1,46 @@
+import cv2 
+
+thres = 0.45
+
+classNames= []
+classFile = 'coco.names'
+with open(classFile, 'rt') as f: 
+	classNames = f.read().rstrip('\n').split('\n') # Seperate at each new line
+
+configPath = 'ssd_mobilenet_v3_large_coco_2020_01_14.pbtxt'
+weightsPath = 'frozen_inference_graph.pb'
+
+net = cv2.dnn_DetectionModel(configPath, weightsPath)
+net.setInputSize(320,320)
+net.setInputScale(1.0/127.5)
+net.setInputMean((127.5,127.5,127.5))
+net.setInputSwapRB(True)
+
+# Returns bounding boxes and object information
+def getObjects(img, draw = True, objects=[]): 
+	classIds, confs, bbox = net.detect(img, confThreshold = thres, nmsThreshold = 0.2)
+	# print(classIds,bbox)
+	if len (objects) == 0:
+		objects = classNames
+	objectInfo = []
+	if len(classIds) != 0:
+		for classId, confidence, box in zip(classIds.flatten(),confs.flatten(),bbox):
+			className = classNames[classId-1]
+			if className in objects:
+				objectInfo.append([box, className])
+				if (draw):
+					cv2.rectangle(img,box,color=(0,225,0),thickness = 2)
+					cv2.putText(img,classNames[classId-1].upper(),(box[0]+10,box[1]+30),cv2.FONT_HERSHEY_COMPLEX,1,(0,255,0),2)
+					cv2.putText(img,str(round(confidence*100,2)),(box[0]+200,box[1]+30),cv2.FONT_HERSHEY_COMPLEX,1,(0,255,0),2)
+	return img, objectInfo
+
+if __name__ == '__main__':
+	cap = cv2.VideoCapture(0)
+	cap.set(3,640)
+	cap.set(4,480)
+	while True:
+		success,img = cap.read()
+		result, ObjectInfo = getObjects(img, True, objects=['mouse'])
+		print(ObjectInfo)
+		cv2.imshow("Output", result)
+		cv2.waitKey(1)
